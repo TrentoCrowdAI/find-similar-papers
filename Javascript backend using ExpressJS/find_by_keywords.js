092a10b5ab3dfb7181a07e8b9e03bb625e7e3a8a
Maurizio Franchi <fmauri90@gmail.com>
2019-11-24 18:51:18 +0100

    Update find_by_keywords.js

---
+++ Javascript backend using ExpressJS/find_by_keywords.js
@@ -0,0 +1,13 @@
+This file has the task of giving the possibility to find similar papers by keywords
+
+The steps are:
+1. receive the xml file with the data of the papers;
+2. read the xml file;
+3. check if there is papers in the xml file
+4. Create a list with paper's title with its authors and publication date
+5. take the first title of the list of the paper;
+6. search the paper in Scopus;
+7. Find the papers that are the same keywords;
+8. Find the authors in the finding papers until there is different authors in the reference;
+9. If there is no different authors in the papers then go to the xml file and cancel the title of the list;
+10. Return to point 4
