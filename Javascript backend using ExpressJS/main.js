a0cbc22cca25853eeb16357d479ac440361f2a64
Muarizio Franchi <fmauri90@gmail.com>
2019-11-26 17:58:54 +0100

    first commit

--- Javascript backend using ExpressJS/main.js
+++
@@ -1,3 +0,0 @@
-This file has the task of giving the possibility to:
-- add the json file with the title of the papers with own authors and year of publication
-- choose how to find similar papers
--- Javascript backend using ExpressJS/src/loaders/index.js
+++
@@ -1,7 +0,0 @@
-const Logger = require('./logger');
-const expressLoader = require('./express');
-
-module.exports = async function(app) {
-  await expressLoader(app);
-  Logger.info('Express loaded');
-};
